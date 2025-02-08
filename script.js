7893f280925663be3840ee7f18b754dd5715467c
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2025-02-08 14:35:13 +0530

    recap of creating a static file in express js

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-const express = require("express");
+const express = require('express')
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.static("./public"));
+app.set("view engine", "ejs")
+app.use(express.static("./public"))
 
-app.get("/", function (req, res) {
-  res.render("index", { age: 12 });
-});
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-app.get("/about", function (req, res) {
-  res.render("about");
-});
-
-
-app.get("/error", function (req, res) {
-    throw Error("Something went wrong")
-});
-  
-
-app.use(function errorHandler(err, req, res, next) {
-  if (res.headersSent) {
-    return next(err);
-  }
-  res.status(500);
-  res.render("error", { error: err });
-});
-
-app.listen(3000);
+app.listen(3000) 
